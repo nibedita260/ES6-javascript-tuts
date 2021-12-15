bef48de8d6769da8d8bab619d55239b19c040a4f
nibedita260 <deepi.nibedita260@gmail.com>
2021-12-15 21:14:36 +0530

    [Nibedita]Added:add objectLiterals in templateLite in Javascript Features Implemented

--- template-litrals.js
+++ template-litrals.js
@@ -6,3 +6,18 @@ console.log(`Fifteen is ${a + b} and
 not ${2 * a + b}.`);
 // "Fifteen is 15 and
 // not 20."
+
+//object literals
+const color = 'red'
+const point = {
+  x: 5,
+  y: 10,
+  color,
+  toString() {
+    return 'X=' + this.x + ', Y=' + this.y + ', color=' + this.color
+  },
+  [ 'prop_' + 42 ]: 42
+}
+
+console.log('The point is ' + point)
+console.log('The dynamic property is ' + point.prop_42)
