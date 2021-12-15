912a5f366e23f98b3d2d303fa1062b491bc09699
nibedita260 <deepi.nibedita260@gmail.com>
2021-12-15 20:39:42 +0530

    [Nibedita]Added:destructing feature in Javascript Features Implemented

---
+++ destruct.js
@@ -0,0 +1,22 @@
+let a, b, rest;
+[a, b] = [10, 20];
+console.log(a); // 10
+console.log(b); // 20
+
+[a, b, ...rest] = [10, 20, 30, 40, 50];
+var arr=[a,b,...rest]
+console.log(arr);
+console.log(a); // 10
+console.log(b); // 20
+console.log(rest); // [30, 40, 50]
+
+
+({ a, b } = { a: 10, b: 20 });
+console.log(a); // 10
+console.log(b); // 20
+
+({a, b, ...rest} = {a: 10, b: 20, c: 30, d: 40});
+console.log(a); // 10
+console.log(b); // 20
+console.log(rest); // {c: 30, d: 40}
+
